57537f977710ae1f2cd38c406f92890363693ba4
stanc <stanko_uzunov@yahoo.com>
2023-12-14 21:17:12 +0100

    Find city coordinates with Selenium

---
+++ WeatherApplication/src/main/java/example/Constants.java
@@ -0,0 +1,13 @@
+package example;
+
+public class Constants {
+    public static final String COORDINATES = "coordinates";
+    public static final String GOOGLE_HOME = "https://google.com";
+    public static final String GOOGLE_DECLINE = "W0wltc";
+    public static final String GOOGLE_SEARCH_BOX = "q";
+    public static final String FIRST_SEARCH_RESULT = "yuRUbf";
+    public static final String CITY_TABLE = "/html/body/main/div[2]/div[2]/div[2]/table/tbody";
+    public static final String CITY_LATITUDE = "/html/body/main/div[2]/div[2]/div[2]/table/tbody/tr[2]/td";
+    public static final String CITY_LONGITUDE = "/html/body/main/div[2]/div[2]/div[2]/table/tbody/tr[3]/td";
+
+}
